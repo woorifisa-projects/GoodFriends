0bc728333fcf3c2ead4305bc6bbdab5382582f88
han0224 <dbn0224@gmail.com>
2023-08-14 08:57:57 +0900

    feat: 프로필 클릭시 popover 띄우기

--- frontend/src/layouts/DefaultHeader.vue
+++ frontend/src/layouts/DefaultHeader.vue
@@ -5,18 +5,83 @@
       <div>
         <button>판매하기</button>
       </div>
-      <button id="login">login</button>
+      <div id="login">
+        <div v-if="!isLogin">
+          <button @click="loginBtnOnClick">login</button>
+        </div>
+        <div v-else>
+          <button class="profile" ref="popoverBtn" @click="onClickProfileBtn">000님</button>
+          <div v-if="isPopoverOpen" class="popover" ref="popover">
+            <div class="img">
+              <img src="@/assets/tmp/images/image.png" alt="예시 이미지" />
+            </div>
+            <div class="content">
+              <div>nickname</div>
+              <div>mypage</div>
+              <div>logout</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watchEffect } from 'vue';
+
+// TODO: login 구현후 수정
+const isLogin = ref(true);
+const isPopoverOpen = ref(false);
+const popover = ref<Element>();
+const popoverBtn = ref<Element>();
+
+const loginBtnOnClick = () => {
+  // TODO: login 구현후 수정
+  isLogin.value = !isLogin.value;
+};
+
+const onClickProfileBtn = (event: MouseEvent) => {
+  if (isPopoverOpen.value) {
+    isPopoverOpen.value = false;
+    closePopover(event);
+  } else {
+    openPopover();
+  }
+};
+
+const closePopover = (event: MouseEvent) => {
+  const target: Element = event.target as Element;
+  if (
+    target.parentElement === popover.value?.parentElement ||
+    target.parentElement === popover.value?.childNodes[0] ||
+    target.parentElement === popover.value?.childNodes[1]
+  ) {
+    return;
+  }
+  if (event.target !== popoverBtn.value) {
+    isPopoverOpen.value = false;
+  }
+};
+
+const openPopover = () => {
+  isPopoverOpen.value = true;
+};
+
+watchEffect(() => {
+  if (isPopoverOpen.value) {
+    window.addEventListener('click', closePopover);
+  } else {
+    window.removeEventListener('click', closePopover);
+  }
+});
+</script>
 
 <style scoped>
 .header {
   width: 100%;
-  height: 64px;
-  padding: 0 20px;
+  height: 80px;
+  padding: 0 250px;
 
   position: sticky;
   top: 0;
@@ -45,5 +110,63 @@
 
 .wrap button {
   font-size: 16px;
+}
+
+#login {
+  width: 60px;
+}
+
+.popover {
+  position: absolute;
+  top: 100px;
+  right: 250px;
+
+  background: wheat;
+  border-radius: 20px;
+
+  padding: 20px;
+
+  width: 200px;
+  height: 300px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.popover::after {
+  border-bottom: 10px solid wheat;
+  border-left: 10px solid transparent;
+  border-right: 10px solid transparent;
+  border-top: 0px solid transparent;
+  content: '';
+  position: absolute;
+  bottom: 300px;
+  right: 20px;
+}
+
+.popover > .img {
+  width: 150px;
+  height: 150px;
+  border-radius: 100%;
+  border: 1px solid black;
+  margin-bottom: 10px;
+  overflow: hidden;
+}
+
+.img > img {
+  height: 100%;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 100%;
+}
+.content > div {
+  width: 100%;
+  border-bottom: 1px solid black;
+  font-size: 18px;
+  text-align: center;
 }
 </style>
