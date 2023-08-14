5305219d9d1d519d4fa5497f5e27ff56ae78b9e0
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-08-14 23:30:15 +0900

    chore: 패키지 경로 수정

--- backend/src/main/java/woorifisa/goodfriends/backend/BackendApplication.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package woorifisa.sut.backend;
+package woorifisa.goodfriends.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
