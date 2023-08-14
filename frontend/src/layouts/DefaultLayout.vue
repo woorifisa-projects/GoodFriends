c39f48c60d0e3a8f5f270d3c7c34d93597caae4b
han0224 <dbn0224@gmail.com>
2023-08-14 09:36:27 +0900

    feat: 기본적인 Layout 구현

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <DefaultHeader />
+    <RouterView />
+    <DefaultFooter />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import DefaultHeader from '@/layouts/DefaultHeader.vue';
+import DefaultFooter from '@/layouts/DefaultFooter.vue';
+</script>
+
+<style scoped></style>
