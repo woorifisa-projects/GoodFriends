3d7cb017032db6a4dadd7a4e9ad46f831930c7f8
YUNSEO HAN <dbn0224@gmail.com>
2023-08-15 16:57:58 +0900

    refactor: 아이템 리스트를 컴포넌트로 분리

---
+++ frontend/src/components/profile/ItemList.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="wrap">
+    <ul>
+      <li>
+        <div class="item" v-for="item in props.items" :key="item.id" @click="onClick(item)">
+          <div class="img">
+            <img src="@/assets/tmp/images/image.png" alt="" />
+          </div>
+          <div class="detail">
+            <div>{{ item.name }}</div>
+            <div>{{ item.date }}</div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Item } from '@/types/Profile';
+
+const props = defineProps({
+  items: {
+    type: Array<Item>,
+    required: true
+  }
+});
+
+const onClick = (item: Item) => {
+  console.log(item);
+  // TODO: 상세 페이지 완성 후 수정 + 삭제 기능 추가할 시 삭제 및 페이지 이동으로 수정
+};
+</script>
+
+<style scoped>
+.wrap {
+  margin: 20px;
+}
+
+.wrap > ul {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
+.item {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  padding: 12px 20px;
+  width: 100%;
+  border: 1px solid rgb(235, 235, 235);
+  border-radius: 10px;
+  box-shadow: 1px 1px 7px #0000001b;
+}
+
+.item > .img {
+  width: 100px;
+  height: 100px;
+  overflow: hidden;
+  border: 1px solid gray;
+  border-radius: 20px;
+}
+
+.img > img {
+  height: 100%;
+}
+
+.detail {
+  display: flex;
+  justify-content: space-around;
+  border: 1px solid gray;
+  height: 100px;
+  width: calc(100% - 100px);
+  border-radius: 20px;
+  overflow: hidden;
+}
+
+.detail > div {
+  background-color: rgb(232, 232, 232);
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  text-align: center;
+  line-height: 58px;
+  font-size: 24px;
+}
+</style>
--- frontend/src/views/PurchaseView.vue
+++ frontend/src/views/PurchaseView.vue
@@ -1,122 +1,21 @@
 <template>
   <DefaultMyPage>
-    <div class="wrap">
-      <ul>
-        <li>
-          <div class="item">
-            <div class="img">
-              <img src="@/assets/tmp/images/image.png" alt="" />
-            </div>
-            <div class="detail">
-              <div>판매 물품 명</div>
-              <div>yyyy-mm-dd</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="item">
-            <div class="img">
-              <img src="@/assets/tmp/images/image.png" alt="" />
-            </div>
-            <div class="detail">
-              <div>판매 물품 명</div>
-              <div>yyyy-mm-dd</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="item">
-            <div class="img">
-              <img src="@/assets/tmp/images/image.png" alt="" />
-            </div>
-            <div class="detail">
-              <div>판매 물품 명</div>
-              <div>yyyy-mm-dd</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="item">
-            <div class="img">
-              <img src="@/assets/tmp/images/image.png" alt="" />
-            </div>
-            <div class="detail">
-              <div>판매 물품 명</div>
-              <div>yyyy-mm-dd</div>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="item">
-            <div class="img">
-              <img src="@/assets/tmp/images/image.png" alt="" />
-            </div>
-            <div class="detail">
-              <div>판매 물품 명</div>
-              <div>yyyy-mm-dd</div>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
+    <ItemList :items="tmpItemList" />
   </DefaultMyPage>
 </template>
 
 <script setup lang="ts">
 import DefaultMyPage from '@/components/profile/DefaultMyPage.vue';
+import ItemList from '@/components/profile/ItemList.vue';
+import type { Item } from '@/types/Profile';
+import { ref } from 'vue';
+
+const tmpItemList = ref<Array<Item>>([
+  { id: 1, name: '판매 물품명1', date: 'yyyy-mm-dd', path: '/profile/purchase' },
+  { id: 2, name: '판매 물품명2', date: 'yyyy-mm-dd', path: '/profile/purchase' },
+  { id: 3, name: '판매 물품명3', date: 'yyyy-mm-dd', path: '/profile/purchase' },
+  { id: 4, name: '판매 물품명4', date: 'yyyy-mm-dd', path: '/profile/purchase' }
+]);
 </script>
 
-<style scoped>
-.wrap {
-  margin: 20px;
-}
-
-.wrap > ul {
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-}
-
-.item {
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  padding: 12px 20px;
-  width: 100%;
-  border: 1px solid rgb(235, 235, 235);
-  border-radius: 10px;
-  box-shadow: 1px 1px 7px #0000001b;
-}
-
-.item > .img {
-  width: 100px;
-  height: 100px;
-  overflow: hidden;
-  border: 1px solid gray;
-  border-radius: 20px;
-}
-
-.img > img {
-  height: 100%;
-}
-
-.detail {
-  display: flex;
-  justify-content: space-around;
-  border: 1px solid gray;
-  height: 100px;
-  width: calc(100% - 100px);
-  border-radius: 20px;
-  overflow: hidden;
-}
-
-.detail > div {
-  background-color: rgb(232, 232, 232);
-  width: 100%;
-  height: 100%;
-  padding: 20px;
-  text-align: center;
-  line-height: 58px;
-  font-size: 24px;
-}
-</style>
+<style scoped></style>
