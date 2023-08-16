6263dd1b4719a66d0ee60beba4583c03e3bb81fe
han0224 <dbn0224@gmail.com>
2023-08-16 16:23:51 +0900

    style: 상품 등록 페이지 디자인

--- frontend/src/views/AddProduct.vue
+++ frontend/src/views/AddProduct.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="add-product">
     <div class="title">내 물건 팔기</div>
-    <div class="button">
-      <button>임시저장</button>
-    </div>
     <div class="form">
-      <div class="imgs">imgs</div>
+      <div class="button">
+        <button>임시저장</button>
+      </div>
+      <div class="imgs">
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+        <div class="img">
+          <img src="@/assets/tmp/images/image.png" alt="" />
+        </div>
+      </div>
       <div class="input-detail">
         <form action="">
           <div class="item">
@@ -47,7 +78,7 @@ import { ref } from 'vue';
 const categories = ref<Array<category>>([
   {
     id: 1,
-    name: '1'
+    name: '가전'
   },
   {
     id: 2,
@@ -68,4 +99,83 @@ const categories = ref<Array<category>>([
 ]);
 </script>
 
-<style scoped></style>
+<style scoped>
+.add-product {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 400px !important;
+}
+.title {
+  font-size: 36px;
+  font-weight: 700;
+
+  margin: 24px 0;
+}
+.button {
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  margin-bottom: 16px;
+}
+.button > button {
+  background-color: lightgray;
+  padding: 16px 24px;
+  border: 1px solid black;
+  border-radius: 16px;
+}
+.form {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  border: 1px solid rgba(197, 197, 197, 0.593);
+  border-radius: 16px;
+  box-shadow: 1px 1px 10px rgba(174, 174, 174, 0.666);
+  padding: 24px;
+
+  margin-bottom: 40px;
+}
+.imgs {
+  overflow-x: auto;
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 12px;
+  padding: 0 24px;
+  margin-bottom: 12px;
+}
+.img {
+  flex: 0 0 auto;
+  width: 150px;
+  height: 150px;
+  border: 1px solid black;
+  border-radius: 16px;
+  overflow: hidden;
+}
+.img > img {
+  width: 100%;
+}
+.input-detail {
+}
+
+.input-detail > form {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+}
+.item {
+  display: flex;
+
+  font-size: 24px;
+}
+
+.item > label {
+  width: 160px;
+}
+.item > input,
+.item > select,
+.item > textarea {
+  width: calc(100% - 160px);
+}
+</style>
