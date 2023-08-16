849495bba50a08a91e3ba64a40abbcc21be45376
han0224 <dbn0224@gmail.com>
2023-08-16 13:55:59 +0900

    feat: router 설정

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,6 +5,8 @@ import ProfileView from '@/views/ProfileView.vue';
 import PurchaseView from '@/views/PurchaseView.vue';
 import SellView from '@/views/SellView.vue';
 import AdminLogin from '@/views/AdminLogin.vue';
+import AddProduct from '@/views/AddProduct.vue';
+import EditProduct from '@/views/EditProduct.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +19,9 @@ const router = createRouter({
         { path: '/', name: 'home', component: HomeView },
         { path: 'profile', name: 'profile', component: ProfileView },
         { path: 'profile/purchase', name: 'purchase', component: PurchaseView },
-        { path: 'profile/sell', name: 'sell', component: SellView }
+        { path: 'profile/sell', name: 'sell', component: SellView },
+        { path: 'product/add/:id', name: 'add product', component: AddProduct },
+        { path: 'product/edit/:id', name: 'edit product', component: EditProduct }
       ]
     },
     {
---
+++ frontend/src/views/EditProduct.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>edit product</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
