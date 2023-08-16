2eac316b6c99241d2eb1b858488508551cb9a996
yybeen <byb09172@naver.com>
2023-08-16 11:33:21 +0900

    feat: 토큰을 이용한 인가를 위한 jwtFilter 구조 생성

---
+++ backend/src/main/java/woorifisa/goodfriends/backend/global/config/utils/JwtFilter.java
@@ -0,0 +1,52 @@
+package woorifisa.goodfriends.backend.global.config.utils;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+import woorifisa.goodfriends.backend.admin.application.AdminService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class JwtFilter extends OncePerRequestFilter { // 매번 인증해야하기 때문에
+
+    private final AdminService adminService;
+    private final String secretKey;
+
+    public JwtFilter(AdminService adminService, String secretKey) {
+        this.adminService = adminService;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // token 안보내면 block 구현해야함
+
+
+        // token 꺼내기 구현해야함
+
+
+        // token 만료 여부 확인 구현해야함
+
+        // adminId를 token에서 꺼내기 구현해야함
+        String adminId = "";
+
+        // 권한 부여
+        // token에 있는 adminId를 꺼내서 아래와 같이 넣어주면 controller에서 adminId 사용 가능
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(adminId, null, List.of(new SimpleGrantedAuthority("Admin"))); // DB에 권한(role) 관련 칼럼 있다면 꺼내 올 수도 있음
+
+        // Detail을 넣어줌
+        // filterChain에 request 넘겨주면 인증되었다고 기록됨 > 다음 기능으로 넘어갈 수 있음
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}
--- backend/src/main/java/woorifisa/goodfriends/backend/global/config/webconfig/AuthenticationConfig.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/global/config/webconfig/AuthenticationConfig.java
@@ -1,15 +1,29 @@
 package woorifisa.goodfriends.backend.global.config.webconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import woorifisa.goodfriends.backend.admin.application.AdminService;
+import woorifisa.goodfriends.backend.global.config.utils.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
 public class AuthenticationConfig {
+
+    private final AdminService adminService;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public AuthenticationConfig(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     // SpringSecurity 적용하면 모든 api에 인증이 필요하다고 default로 호출됨
     // 로그인이나 회원가입같이 인증받지 않고 접속해야하는 부분 설정할 수 있는 부분
 
@@ -23,6 +37,7 @@ public class AuthenticationConfig {
                 .antMatchers("/admin/login").permitAll() // 로그인은 언제나 가능
                 .antMatchers(HttpMethod.POST, "/admin/**").authenticated()
                 .and()
+                .addFilterBefore(new JwtFilter(adminService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
 
     }
