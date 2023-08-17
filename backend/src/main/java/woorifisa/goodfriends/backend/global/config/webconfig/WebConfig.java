c7b5e54a042d2293263466797046f2c72fa42b60
yybeen <byb09172@naver.com>
2023-08-17 19:47:08 +0900

    feature: 프론트와 연동을 위한 webConfig 파일 추가

---
+++ backend/src/main/java/woorifisa/goodfriends/backend/global/config/webconfig/WebConfig.java
@@ -0,0 +1,17 @@
+package woorifisa.goodfriends.backend.global.config.webconfig;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET","POST","PUT","DELETE");
+    }
+}
