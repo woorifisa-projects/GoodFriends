a5679b57ee41cbf4064e46337ea39ea1bc61e3fd
han0224 <dbn0224@gmail.com>
2023-08-18 16:48:20 +0900

    refactor: 불필요한 주석 삭제

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -2,6 +2,5 @@ export const dateFormat = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
-  console.log(year, month, day);
   return `${year}-${`0${month}`.slice(-2)}-${`0${day}`.slice(-2)}`;
 };
