e21d7d98abce076fa692d898edd41ed679f10d2e
han0224 <dbn0224@gmail.com>
2023-08-19 14:35:25 +0900

    feat: type props추가
    해당 props로 수정인지, 등록인지 구별

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -2,3 +2,11 @@ export interface category {
   id: number;
   name: string;
 }
+
+export interface productEdit {
+  price: number;
+  name: string;
+  content: string;
+  category: number;
+  date: Date;
+}
