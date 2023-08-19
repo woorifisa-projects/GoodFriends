f0aa3bb0b1b87b884cb1696b003a284390d43110
han0224 <dbn0224@gmail.com>
2023-08-19 14:43:58 +0900

    fix: month 출력 수정
    getMonth는 0~11로 +1을 해주어야 현재 월과 동일

--- frontend/src/utils/format.ts
+++ frontend/src/utils/format.ts
@@ -2,5 +2,6 @@ export const dateFormat = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
-  return `${year}-${`0${month}`.slice(-2)}-${`0${day}`.slice(-2)}`;
+
+  return `${year}-${`0${month + 1}`.slice(-2)}-${`0${day}`.slice(-2)}`;
 };
