bd1e5343712d0a0eb0d66c3151940e58f34e541f
han0224 <dbn0224@gmail.com>
2023-08-19 14:39:46 +0900

    refactor: 페이지가 아닌 컴포넌트에서 함수 호출

--- frontend/src/views/EditProductView.vue
+++ frontend/src/views/EditProductView.vue
@@ -1,13 +1,9 @@
 <template>
-  <EditProduct @submit="submit" type="edit" />
+  <EditProduct type="edit" />
 </template>
 
 <script setup lang="ts">
 import EditProduct from '@/components/product/EditProduct.vue';
-
-const submit = (obj) => {
-  console.log('parent', obj);
-};
 </script>
 
 <style scoped></style>
