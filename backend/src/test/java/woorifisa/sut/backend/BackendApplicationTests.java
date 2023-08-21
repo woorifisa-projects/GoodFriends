fc5c1fafb1510ec947697c6479866ac6a80c145c
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-08-21 10:37:42 +0900

    스프링 부트 버전 변경 및 테스트 관련 자동 설정 기능 제공 (#59)
    * chore: mysql 관련 yml 파일 .gitignore에 추가
    
    * chore: springBoot version 2.7.1로 수정
    
    * chore: 테스트 관련 자동 설정 기능을 제공하기 위해 @SpringBootConfiguration 추가

--- backend/src/test/java/woorifisa/sut/backend/BackendApplicationTests.java
+++ backend/src/test/java/woorifisa/sut/backend/BackendApplicationTests.java
@@ -1,9 +1,11 @@
 package woorifisa.sut.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class BackendApplicationTests {
 
 	@Test
