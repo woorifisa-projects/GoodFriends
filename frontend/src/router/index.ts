1111adcbad5a6cfb8a8f84dd42ac919e4f25dedd
han0224 <dbn0224@gmail.com>
2023-08-21 16:18:21 +0900

    feat: 페이지 이동시 스크롤 위치 상단으로 고정

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -10,6 +10,9 @@ import EditProduct from '@/views/EditProductView.vue';
 import ProductView from '@/views/ProductView.vue';
 
 const router = createRouter({
+  scrollBehavior() {
+    return { top: 0 };
+  },
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
