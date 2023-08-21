5a83208705bc94bde82f606c06d51908f93cd81c
han0224 <dbn0224@gmail.com>
2023-08-21 16:34:05 +0900

    feat: 상품 이미지가 여러개일 경우 버튼으로 이미지 변경

--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -2,7 +2,16 @@
   <div class="product-page">
     <div class="product-info">
       <div class="imgs">
-        <img src="@/assets/tmp/images/image.png" alt="" />
+        <button @click="onClickBannerBtn('prev')" :disabled="viewImage === 0">
+          <span class="material-icons-outlined"> arrow_back_ios </span>
+        </button>
+        <img :src="product.images[viewImage]" alt="" />
+        <button
+          @click="onClickBannerBtn('next')"
+          :disabled="viewImage === product.images.length - 1"
+        >
+          <span class="material-icons-outlined"> arrow_forward_ios </span>
+        </button>
       </div>
       <div class="info">
         <div v-if="isWriter" class="wrap-btn">
@@ -13,10 +22,10 @@
           <button @click="onClickReport">신고하기</button>
         </div>
         <div class="detail-info">
-          <div class="name">0000 팝니다~</div>
-          <div class="price">100000 원</div>
-          <div class="category">category</div>
-          <div class="date">등록날짜: yyyy-mm-dd</div>
+          <div class="name">{{ product.title }}~</div>
+          <div class="price">{{ product.price }}원</div>
+          <div class="category">{{ product.category }}</div>
+          <div class="date">등록날짜: {{ product.createdAt }}</div>
         </div>
       </div>
     </div>
@@ -35,13 +44,32 @@
 import router from '@/router';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import image from '@/assets/tmp/images/image.png';
+import banner1 from '@/assets/tmp/images/banner1.jpeg';
 
 const route = useRoute();
 
 const id = ref(route.params.id);
 // TODO: 작성자 인지 아닌지 -> API 연결 필요
 const isWriter = ref(Math.random() < 0.5);
+// TODO: API 연결
+const product = ref({
+  title: '상품 제목',
+  description: '상세 내용',
+  createdAt: 'yyyy-mm-dd',
+  category: 'category',
+  price: 1000,
+  images: [image, banner1]
+});
+const viewImage = ref(0);
 
+const onClickBannerBtn = (flag: string) => {
+  if (flag === 'next') {
+    viewImage.value = viewImage.value + 1;
+  } else if (flag === 'prev') {
+    viewImage.value -= 1;
+  }
+};
 const onClickEditBtn = () => {
   router.push('/product/edit/' + id.value);
 };
@@ -68,7 +96,12 @@ const onClickReport = () => {
 .imgs {
   flex: 1;
 
-  width: 100%;
+  width: 450px;
+  height: 450px;
+
+  display: flex;
+  align-items: center;
+  position: relative;
 
   overflow: hidden;
   text-align: center;
@@ -80,6 +113,17 @@ const onClickReport = () => {
   border: 1px solid rgba(0, 0, 0, 0.278);
   border-radius: 8px;
 }
+.imgs > button {
+  position: absolute;
+  background-color: transparent;
+}
+.imgs > button:first-child {
+  left: 0;
+}
+.imgs > button:last-child {
+  right: 0;
+}
+
 .imgs > img {
   width: 100%;
   object-fit: cover;
