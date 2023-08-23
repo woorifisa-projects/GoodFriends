7c2aaba02113a5e38e1499e1bec8d7ef57ee1bf5
han0224 <dbn0224@gmail.com>
2023-08-23 18:39:23 +0900

    feat: 주문하기 모달속 text 정리

--- frontend/src/constants/strings/product.ts
+++ frontend/src/constants/strings/product.ts
@@ -10,5 +10,15 @@ export const PRODUCT = {
   CATEGORY: '카테고리',
   PLEASE_SELECT: '선택해주세요',
   CREATE_AT: '등록 날짜',
-  REPORT: '신고하기'
+  REPORT: '신고하기',
+  ORDER: '주문하기'
+};
+
+export const ORDER_MODAL = {
+  TITLE: '주문하기',
+  CONTENTS: [`원하는 날짜와 시간을 입력해주세요`, `추가적인 내용은 아래 요청사항에 적어주세요`],
+  DATE: '날짜: ',
+  TIME: '시간: ',
+  REQUIREMENT: '요청사항',
+  SUBMIT: '주문서 제출하기'
 };
