b514af730289e94678fda03ea382352ebc163692
han0224 <dbn0224@gmail.com>
2023-08-23 10:06:28 +0900

    feat: 모달 밖 클릭시 모달 창 닫기

---
+++ frontend/src/components/CommonModal.vue
@@ -0,0 +1,61 @@
+<template>
+  <Teleport to="body">
+    <div v-if="props.isVisible" ref="back" class="modal-back" @click="closeClick">
+      <div class="modal-front">
+        <slot>
+          <div>hi</div>
+        </slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    required: true
+  }
+});
+
+const emits = defineEmits(['updateVisible']);
+
+const back = ref<Element>();
+
+const closeClick = (e: Event) => {
+  if (e.target === back.value) {
+    emits('updateVisible', false);
+  }
+};
+</script>
+
+<style scoped>
+.modal-back {
+  height: 100vh;
+  width: 100vw;
+
+  z-index: 100;
+  position: fixed;
+  top: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(213, 213, 213, 0.692);
+}
+.modal-front {
+  min-width: 60%;
+  max-width: 89%;
+  min-height: 70%;
+  max-height: 90%;
+
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 16px;
+
+  padding: 24px;
+}
+</style>
