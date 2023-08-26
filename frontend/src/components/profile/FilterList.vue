ef558e73aaabfcd71a3baed0404b1c5906c7457f
YUNSEO HAN <70616579+han0224@users.noreply.github.com>
2023-08-26 14:17:12 +0900

    Feat/102(프로필에서 상품 상태에 따른 필터링) (#106)
    * fix: 오타 수정
    
    * feat: 판매 상태별 필터링
    
    * feat: 구매 상태별 필터링
    
    * fix: 이벤트가 두번 발생하던 오류 수정
    
    * refactor: 주석 삭제
    
    * refactor: 예약 -> 예약중
    
    * style: 색상 변경

---
+++ frontend/src/components/profile/FilterList.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="filter">
+    <div v-for="status in props.productStatus" :key="status.id">
+      <input
+        type="radio"
+        :id="status.value"
+        name="filter"
+        :checked="checkedStatus === status.value"
+        @change="emits('click', status.value)"
+      />
+      <label :for="status.value">
+        {{ status.name }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { filter } from '@/types/product';
+
+const props = defineProps({
+  productStatus: {
+    type: Array<filter>,
+    required: true
+  },
+  checkedStatus: {
+    type: String,
+    required: true
+  }
+});
+
+const emits = defineEmits(['click']);
+</script>
+
+<style scoped>
+.filter {
+  width: 100%;
+  padding: 18px 24px 0px 24px;
+  display: flex;
+  justify-content: center;
+  gap: 24px;
+}
+
+.filter > div {
+  width: 80%;
+  display: flex;
+  border: 1px solid black;
+  border-radius: 12px;
+}
+.filter > div {
+  flex: 1;
+  background-color: lightblue;
+  text-align: center;
+  border-radius: 12px;
+  overflow: hidden;
+  min-width: fit-content;
+  width: 50px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+}
+
+.filter > div > input {
+  display: none;
+  width: 100%;
+  height: 100%;
+}
+.filter > div > input:checked + label {
+  background-color: rgb(36, 71, 246);
+  color: white;
+}
+.filter > div > label {
+  display: block;
+
+  width: 100%;
+  padding: 12px;
+  cursor: pointer;
+}
+</style>
--- frontend/src/views/user/profile/SellView.vue
+++ frontend/src/views/user/profile/SellView.vue
@@ -1,13 +1,19 @@
 <template>
   <DefaultMyPage>
+    <FilterListVue
+      :product-status="productStatus"
+      @click="onClickFilter"
+      :checkedStatus="checkedStatus"
+    />
     <ItemList :items="tmpItemList" />
   </DefaultMyPage>
 </template>
 
 <script setup lang="ts">
 import DefaultMyPage from '@/components/profile/DefaultMyPage.vue';
+import FilterListVue from '@/components/profile/FilterList.vue';
 import ItemList from '@/components/profile/ItemList.vue';
-import type { Item } from '@/types/Profile';
+import type { Item } from '@/types/profile';
 
 import { ref } from 'vue';
 
@@ -17,6 +23,34 @@ const tmpItemList = ref<Array<Item>>([
   { id: 3, name: '판매 물품명3', date: 'yyyy-mm-dd', path: '/profile/sell' },
   { id: 4, name: '판매 물품명4', date: 'yyyy-mm-dd', path: '/profile/sell' }
 ]);
+
+const productStatus = [
+  {
+    id: 0,
+    name: '전체',
+    value: 'all'
+  },
+  {
+    id: 1,
+    name: '판매중',
+    value: 'selling'
+  },
+  {
+    id: 2,
+    name: '예약중',
+    value: 'reservation'
+  },
+  {
+    id: 3,
+    name: '판매 완료',
+    value: 'done'
+  }
+];
+const checkedStatus = ref('all');
+
+const onClickFilter = (status: string) => {
+  console.log('filter: ', status);
+};
 </script>
 
 <style scoped></style>
