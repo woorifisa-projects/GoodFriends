cc01876ce3c040d342f9258640a3b4c5cfac2011
Hmini0101 <59523668+Hmini0101@users.noreply.github.com>
2023-08-28 11:55:13 +0900

    Feature/116([관리자 페이지] Header 컴포넌트를 수정한다) (#117)
    * feat: 사이드바 홈화면 메뉴로고, 로그아웃 기능 추가
    
    * feat: 사이드바 전용 버튼 로고,로그아웃 기능 추가 및 화면 개선

--- frontend/src/layouts/admin/DefaultSide.vue
+++ frontend/src/layouts/admin/DefaultSide.vue
@@ -1,34 +1,50 @@
 <template>
   <div id="wrap">
-    <div class="sidebar">
-      <div class="sidebar-inner">
-        <div class="logo">
-          <div class="logo-name">
-            <button class="logo-name-btn" @click="onClickMangeLog">{{ ADMIN_SIDEBAR.LOGO }}</button>
-          </div>
-          <div class="logo-admin">{{ ADMIN_SIDEBAR.MANAGER }}</div>
-        </div>
-        <div class="admin-menu">
-          <ul class="admin-menu-ul">
-            <li>
-              <button @click="onClickMangeLog">{{ ADMIN_SIDEBAR.MANAGE_LOG }}</button>
-            </li>
-            <li>
-              <button @click="onClickManegeUser">{{ ADMIN_SIDEBAR.MANAGE_USER }}</button>
-            </li>
-            <li>
-              <button @click="onClickManegeProduct">{{ ADMIN_SIDEBAR.MANAGE_PRODUCT }}</button>
-            </li>
-            <li>
-              <button @click="onClickManegeDeclaration">
-                {{ ADMIN_SIDEBAR.MANAGE_DECLAREATION }}
+    <div class="header">
+      <button @click="onClikeSidebarIcon">
+        <span class="material-icons-outlined"> density_medium </span>
+      </button>
+      <button @click="onClickLogout">로그아웃</button>
+    </div>
+    <div class="total-body">
+      <div class="sidebar">
+        <div class="sidebar-inner">
+          <div class="logo">
+            <div class="logo-name">
+              <button class="logo-name-btn" @click="onClickMangeLog">
+                {{ ADMIN_SIDEBAR.LOGO }}
               </button>
-            </li>
-          </ul>
+            </div>
+            <div class="logo-admin">{{ ADMIN_SIDEBAR.MANAGER }}</div>
+          </div>
+          <div class="admin-menu">
+            <ul class="admin-menu-ul">
+              <li>
+                <button class="side-button" @click="onClickMangeLog">
+                  {{ ADMIN_SIDEBAR.MANAGE_LOG }}
+                </button>
+              </li>
+              <li>
+                <button class="side-button" @click="onClickManegeUser">
+                  {{ ADMIN_SIDEBAR.MANAGE_USER }}
+                </button>
+              </li>
+              <li>
+                <button class="side-button" @click="onClickManegeProduct">
+                  {{ ADMIN_SIDEBAR.MANAGE_PRODUCT }}
+                </button>
+              </li>
+              <li>
+                <button class="side-button" @click="onClickManegeDeclaration">
+                  {{ ADMIN_SIDEBAR.MANAGE_DECLAREATION }}
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+      <RouterView id="main" />
     </div>
-    <RouterView id="main" />
   </div>
 </template>
 
@@ -36,7 +52,20 @@
 import { ADMIN, ADMIN_SIDEBAR } from '@/constants/strings/admin';
 import router from '@/router';
 
+const onClikeSidebarIcon = () => {
+  const con = document.getElementsByClassName('sidebar')[0] as HTMLElement;
+
+  if (con.style.display === 'none') {
+    con.style.display = 'block';
+  } else {
+    con.style.display = 'none';
+  }
+};
+
 //TODO: 페이지 이동
+const onClickLogout = () => {
+  router.push('/admin');
+};
 const onClickMangeLog = () => {
   router.push('/admin/log');
 };
@@ -53,14 +82,38 @@ const onClickManegeDeclaration = () => {
 <style scoped>
 #wrap {
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
+  flex-direction: column;
+  padding: 5px;
+}
+.header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+.material-symbols-outlined {
+  font-variation-settings:
+    'FILL' 0,
+    'wght' 400,
+    'GRAD' 0,
+    'opsz' 48;
+}
+.total-body {
+  display: flex;
+  height: calc(100vh - 30px);
+  gap: 5px;
 }
 
 #main {
   width: 100%;
   height: 100%;
   padding: 10px;
+  box-shadow: 1px 1px 10px rgba(168, 168, 168, 0.596);
+  border: 1px solid rgb(173, 173, 173);
+  border-radius: 16px;
 }
 .sidebar {
   /* position: relative; */
@@ -69,6 +122,9 @@ const onClickManegeDeclaration = () => {
   width: 200px;
   height: 100%;
   background-color: dodgerblue;
+  box-shadow: 1px 1px 10px rgba(168, 168, 168, 0.596);
+  border: 1px solid rgb(173, 173, 173);
+  border-radius: 16px;
 }
 .sidebar-inner {
   padding-top: 100px;
@@ -79,6 +135,7 @@ const onClickManegeDeclaration = () => {
   padding-left: 10px;
   font-size: 21px;
   font-weight: bold;
+  background-color: dodgerblue;
 }
 
 .logo-admin {
@@ -90,12 +147,12 @@ const onClickManegeDeclaration = () => {
   padding-top: 50px;
   text-align: center;
 }
-s ul {
+ul {
   display: flex;
   flex-direction: column;
   gap: 20px;
 }
-button {
+.side-button {
   height: 30px;
   font-size: 18px;
   font-weight: bold;
