432c6d236d08f97257020baad70a60f8af97ff46
Hmini0101 <59523668+Hmini0101@users.noreply.github.com>
2023-08-28 12:31:59 +0900

    Feature/114([관리자 페이지] 사용자 관리 페이지를 개발한다.) (#120)
    * feat: 사이드바 홈화면 메뉴로고, 로그아웃 기능 추가
    
    * feat: 사이드바 전용 버튼 로고,로그아웃 기능 추가 및 화면 개선
    
    * feat: 유저관리페이지 기본 틀 구현
    
    * feat: 사용자관리 페이지 기능 구현
    
    사이드바 클릭시 이동가능, 리스트별 사용자 id으로 이동가능
    
    * feat: 신고관리페이지 이동 기능, 리스트업 구현
    
    * style: 사용자정보 항목넓이 수정
    
    * style: 공통컴포넌트 테이블 가운데정렬, 헤더 폰트 크기 조정
    
    * refactor: 테이블 컴포넌트 전달값 코드 수정
    
    * feat: 사용자관리 페이지에서의 디테일 화면 이동 구현
    
    관리 페이지에서 특정 사용자 리스트 클릭시 해당 사용자로 이동 및 데이터 전달
    
    * refactor: 코드 오류 수정
    
    * refactor: 주석 제거 및 수정
    
    * refactor: 글자간 공백 특수문자를 span요소로 변경

--- frontend/src/constants/strings/admin.ts
+++ frontend/src/constants/strings/admin.ts
@@ -1,35 +1,36 @@
 export const ADMIN_SIDEBAR = {
-    LOGO : 'GoodFriends',
-    MANAGER : '관리자',
-    MANAGE_LOG : '로그 관리',
-    MANAGE_USER : '사용자 관리',
-    MANAGE_PRODUCT : '상품 관리',
-    MANAGE_DECLAREATION: '신고 관리'
-}
+  LOGO: 'GoodFriends',
+  MANAGER: '관리자',
+  MANAGE_LOG: '로그 관리',
+  MANAGE_USER: '사용자 관리',
+  MANAGE_PRODUCT: '상품 관리',
+  MANAGE_DECLAREATION: '신고 관리'
+};
 
 export const ADMIN = {
-    EDIT_USER_TITLE: '사용자 정보 수정',
-    BAN: '신고당한 횟수',
-    MARK: '인증 뱃지',
-    AVERRAGE: '평점',
-    EMAIL: '이메일',
-    NICKNAME: '이름',
-    SEX: '성별',
-    PHONE: '연락처',
-    ADDRESS: '주소',
-    SUSPEND: '활동 정지',
-    EDIT: '수정 완료'
-  };
+  EDIT_USER_TITLE: '정보 수정',
+  BAN: '신고당한 횟수',
+  MARK: '인증 뱃지',
+  AVERRAGE: '평점',
+  EMAIL: '이메일',
+  NICKNAME: '닉네임',
+  SEX: '성별',
+  PHONE: '연락처',
+  ADDRESS: '주소',
+  SUSPEND: '활동 정지',
+  EDIT: '수정 완료'
+};
 
-  export const ADMIN_PRODUCT = {
-    PRODUCT_TITLE_ADD: '상품 등록',
-    PRODUCT_TITLE: '상품 제목',
-    PRODCUT_DEFAULT_CATEGORY : '선택해주세요',
-    PRODUCT_CATEGORY: '카테고리',
-    PRODUCT_PRICE: '가격',
-    PRODUCT_SELL_DATE: '판매일자',
-    PRODUCT_DESCRIPTION: '상품설명',
-    PRODUCT_EDIT: '수정하기',
-    PRODUCT_DELETE: '삭제하기',
-    PRODUCT_ADD: '등록하기',
-    PRODUCT_CANCEL: '취소하기'}
+export const ADMIN_PRODUCT = {
+  PRODUCT_TITLE_ADD: '상품 등록',
+  PRODUCT_TITLE: '상품 제목',
+  PRODCUT_DEFAULT_CATEGORY: '선택해주세요',
+  PRODUCT_CATEGORY: '카테고리',
+  PRODUCT_PRICE: '가격',
+  PRODUCT_SELL_DATE: '판매일자',
+  PRODUCT_DESCRIPTION: '상품설명',
+  PRODUCT_EDIT: '수정하기',
+  PRODUCT_DELETE: '삭제하기',
+  PRODUCT_ADD: '등록하기',
+  PRODUCT_CANCEL: '취소하기'
+};
