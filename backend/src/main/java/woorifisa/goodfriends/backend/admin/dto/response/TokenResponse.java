27f961f22da6b664459ae520bd54b24a57a04d8d
yybeen <byb09172@naver.com>
2023-08-29 18:13:00 +0900

    fix: 관리자 및 상품 api response 변경 -> TokenResponse 추가

---
+++ backend/src/main/java/woorifisa/goodfriends/backend/admin/dto/response/TokenResponse.java
@@ -0,0 +1,16 @@
+package woorifisa.goodfriends.backend.admin.dto.response;
+
+public class TokenResponse {
+    private String accessToken;
+
+    public TokenResponse() {
+    }
+
+    public TokenResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
