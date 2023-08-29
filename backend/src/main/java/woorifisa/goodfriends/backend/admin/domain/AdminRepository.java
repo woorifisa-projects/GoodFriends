4bb09ba6954c007156ab5e56f16d4551037bdd44
yybeen <59864345+yybeen@users.noreply.github.com>
2023-08-29 19:11:47 +0900

    Feature/141([관리자] 상품 등록/조회/수정/삭제 기능을 구현한다.) (#151)
    * fix: admin 아이디 칼러명 admin_id에서 root로 수정
    
    * fix: Product 테이블에 관리자 테이블 Foreign Key로 추가
    
    * feat: 관리자 상품 등록 기능 구현
    
    * feat: 관리자 전체 상품 조회 기능 구현
    
    * feat: 관리자 선택한 상품 상세 조회 기능 구현
    
    * feat: 관리자 수정할 상품 정보 조회 및 수정 기능 구현
    
    * feat: 관리자 상품 삭제 기능 구현
    
    * fix: 관리자 및 상품 api response 변경
    
    * fix: 관리자 및 상품 api response 변경 -> TokenResponse 추가

--- backend/src/main/java/woorifisa/goodfriends/backend/admin/domain/AdminRepository.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/admin/domain/AdminRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 @Repository
 public interface AdminRepository extends CrudRepository<Admin, Long> {
 
-    Optional<Admin> findByAdminId(String adminId);
+    Optional<Admin> findByRoot(String adminId);
 }
--- backend/src/main/java/woorifisa/goodfriends/backend/admin/presentation/AdminController.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/admin/presentation/AdminController.java
@@ -1,15 +1,26 @@
 package woorifisa.goodfriends.backend.admin.presentation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import woorifisa.goodfriends.backend.admin.application.AdminService;
 import woorifisa.goodfriends.backend.admin.dto.request.AdminLoginRequest;
+import woorifisa.goodfriends.backend.admin.dto.response.TokenResponse;
+import woorifisa.goodfriends.backend.product.dto.request.ProductSaveRequest;
+import woorifisa.goodfriends.backend.product.dto.request.ProductUpdateRequest;
+import woorifisa.goodfriends.backend.product.dto.response.ProductSaveResponse;
+import woorifisa.goodfriends.backend.product.dto.response.ProductUpdateResponse;
+import woorifisa.goodfriends.backend.product.dto.response.ProductViewAllResponse;
+import woorifisa.goodfriends.backend.product.dto.response.ProductViewOneResponse;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
 
 @RestController
-@RequestMapping("/admin")
+@RequestMapping("api/admin/")
 public class AdminController {
 
     private final AdminService adminService;
@@ -19,9 +30,54 @@ public class AdminController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody AdminLoginRequest adminLoginRequest) {
+    public ResponseEntity<TokenResponse> login(@RequestBody AdminLoginRequest adminLoginRequest) {
 
-        String token = adminService.login(adminLoginRequest.getAdminId(), adminLoginRequest.getPassword());
-        return ResponseEntity.ok().body(token);
+        TokenResponse tokenResponse = adminService.login(adminLoginRequest.getRoot(), adminLoginRequest.getPassword());
+
+        return ResponseEntity.ok().body(tokenResponse);
     }
+
+    @PostMapping("/products/new")
+    public ResponseEntity<Long> saveProduct(Authentication authentication,
+                                                           @RequestPart ProductSaveRequest request,
+                                                           @RequestPart List<MultipartFile> multipartFiles) throws IOException {
+        String adminId = authentication.getName();
+        ProductSaveRequest productSaveRequest = new ProductSaveRequest(request.getTitle(), request.getProductCategory(),request.getDescription(), request.getSellPrice(), multipartFiles);
+        ProductSaveResponse response = adminService.saveProduct(adminId, productSaveRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response.getId());
+    }
+
+    @GetMapping("/products/view")
+    public ResponseEntity<List<ProductViewAllResponse>> viewAllProduct() {
+        List<ProductViewAllResponse> responses = adminService.viewAllProduct();
+        return ResponseEntity.ok().body(responses);
+    }
+
+    @GetMapping("/products/view/{productId}")
+    public ResponseEntity<ProductViewOneResponse> viewOneProduct(@PathVariable Long productId) {
+        ProductViewOneResponse response = adminService.viewOneProduct(productId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/products/edit/{productId}")
+    public ResponseEntity<ProductUpdateResponse> showSelectedProduct(@PathVariable Long productId){
+        ProductUpdateResponse response = adminService.showSelectedProduct(productId);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @PutMapping("/products/edit/{productId}")
+    public ResponseEntity<Void> updateProduct(@PathVariable Long productId,
+                                                               @RequestPart ProductUpdateRequest request,
+                                                               @RequestPart List<MultipartFile> multipartFiles) throws IOException {
+        ProductUpdateRequest productUpdateRequest = new ProductUpdateRequest(request.getTitle(), request.getProductCategory(), request.getDescription(), request.getSellPrice(), multipartFiles);
+        ProductUpdateResponse response = adminService.updateProduct(productUpdateRequest, productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/products/delete/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) throws MalformedURLException {
+        adminService.deleteById(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
