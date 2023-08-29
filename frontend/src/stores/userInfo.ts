75c635e5b171052110cc0fe084e8c8f3d6a82498
han0224 <dbn0224@gmail.com>
2023-08-29 12:24:31 +0900

    feat: 임시 데이터

--- frontend/src/stores/userInfo.ts
+++ frontend/src/stores/userInfo.ts
@@ -1,10 +1,11 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import { tmpImage } from '@/utils/image';
 
 export const useUserInfoStore = defineStore('userInfo', () => {
   const userId = ref(0);
-  const userName = ref('');
-  const profileImg = ref('');
+  const userName = ref('임시닉네임');
+  const profileImg = ref(tmpImage);
 
   function setUserInfo(id: number, name: string, img: string) {
     userId.value = id;
