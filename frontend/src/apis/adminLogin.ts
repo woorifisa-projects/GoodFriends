c59794618472b8c88ca3fb58b6cff1674c84acd6
han0224 <dbn0224@gmail.com>
2023-08-30 15:58:26 +0900

    feat: admin 로그인 수정

--- frontend/src/apis/adminLogin.ts
+++ frontend/src/apis/adminLogin.ts
@@ -1,38 +1,22 @@
-import axios, { AxiosError } from "axios";
-const BASE_URL = "http://localhost:8080/admin";
-  
-export const login = async (userId: string, userPw: string): Promise<boolean> => {
-    try {
-      const response = await axios.post(
-        BASE_URL+"/login",
-        {
-          // 로그인 창에서 클라이언트가 입력하는 데이터
-          "adminId" : userId,
-          "password" : userPw
-        },
-        {
-          headers : {
-            // 클라이언트가 서버에게 요청하는 타입
-            "Content-Type" : "application/json;charset=UTF-8",
-            // 클라이언트가 서버에게 보내는 타입
-            'Accept' : 'application/json'
-          }
-        }
-      );
+import { AxiosError, type AxiosResponse } from 'axios';
+import { apiInstance } from '.';
+import { useAdminStore } from '@/stores/admin';
 
-      // 로그인 성공 시 토큰을 LocalStorage에 저장
-      localStorage.setItem('accessToken', response.data);
+const api = apiInstance();
 
-      // 요청 헤더에 토큰 추가
-      axios.defaults.headers.common["Authorization"] = "Bearer " + localStorage.getItem('accessToken');
-
-      return true;
-    } 
-    catch(error) {
-      // 에러를 AxiosError 타입으로 캐스팅
-      const e = error as AxiosError;
-      console.log(e.response?.data);
-  
-      return false;
-    }
-  };
+export const AdminLogin = async (id: string, password: string) => {
+  return await api
+    .post('api/admin/login', {
+      root: id,
+      password: password
+    })
+    .then((res: AxiosResponse) => {
+      const { data } = res;
+      const store = useAdminStore();
+      store.setAdmin(id, password, data.accessToken);
+      return { isSuccess: true, message: 'success' };
+    })
+    .catch((error: AxiosError) => {
+      return { isSuccess: false, message: error.message };
+    });
+};
