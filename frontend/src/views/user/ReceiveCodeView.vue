87c52b9aceddc12000e9e45d07684b98041e16ed
han0224 <dbn0224@gmail.com>
2023-08-30 10:21:45 +0900

    feat: redirect 페이지

---
+++ frontend/src/views/user/ReceiveCodeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <LoadingIconVue />
+</template>
+
+<script setup lang="ts">
+import { getAccessToken } from '@/apis/userLogin';
+import LoadingIconVue from '@/components/LoadingIcon.vue';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const getQuery = () => {
+  const code = route.query.code as string;
+  return code || '';
+};
+
+onMounted(async () => {
+  const code = getQuery();
+
+  const res = await getAccessToken(code);
+  if (res.isSuccess) {
+    window.location.href = import.meta.env.BASE_URL;
+  } else {
+    window.location.href = import.meta.env.BASE_URL + '/err/login';
+  }
+});
+</script>
+
+<style scoped></style>
