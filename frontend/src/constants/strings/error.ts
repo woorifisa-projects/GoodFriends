87045d1ae858a3dfa2bd140d393022062fc8f693
han0224 <dbn0224@gmail.com>
2023-08-30 14:37:48 +0900

    refactor: not found page

--- frontend/src/constants/strings/error.ts
+++ frontend/src/constants/strings/error.ts
@@ -2,5 +2,9 @@ export const ERROR = {
   ERROR: '에러',
   MESSAGE: '가 발생했습니다!',
   LOGIN: '로그인에 문제가 생겼습니다. 다시 시도해주세요.',
-  SERVER: '서버에 문제가 생겼습니다. 다시 시도해주세요.'
+  SERVER: '서버에 문제가 생겼습니다. 다시 시도해주세요.',
+  NOT_FOUND: '찾을 수 없는 페이지',
+  NOT_FOUND_MESSAGE: '입니다.',
+  NOT_FOUND_ERR_DETAIL: '요청하신 페이지가 사라졌거나, 잘못된 경로입니다.',
+  GO_HOME: '메인화면으로 이동'
 };
