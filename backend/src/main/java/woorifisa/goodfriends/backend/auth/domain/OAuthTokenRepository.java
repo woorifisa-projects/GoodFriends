460ff9996e9196e6f8eef67711342e250fd3c5f9
devfancy <fancy.junyongmoon@gmail.com>
2023-08-30 16:35:18 +0900

    feat: 로그아웃 기능 구현
    - AuthController에서 로그아웃시 액세스 토큰과 리프레시 토큰을 만료시키는 기능을 구현했습니다.
    -유저 id 값을 찾아서 삭제하는  MySQL 쿼리를 사용했습니다.

--- backend/src/main/java/woorifisa/goodfriends/backend/auth/domain/OAuthTokenRepository.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/auth/domain/OAuthTokenRepository.java
@@ -1,10 +1,12 @@
 package woorifisa.goodfriends.backend.auth.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import woorifisa.goodfriends.backend.auth.exception.NotFoundOAuthTokenException;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 public interface OAuthTokenRepository extends JpaRepository<OAuthToken, Long> {
@@ -15,8 +17,15 @@ public interface OAuthTokenRepository extends JpaRepository<OAuthToken, Long> {
                 .orElseThrow(NotFoundOAuthTokenException::new);
     }
 
+    // MySQL 쿼리를 사용
     @Query("SELECT o "
             + "FROM OAuthToken o "
             + "WHERE o.user.id = :userId")
     Optional<OAuthToken> findByUserId(final Long userId);
+
+    // MySQL 쿼리를 사용
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM OAuthToken o WHERE o.user.id = :userId")
+    void deleteAllByUserId(Long userId);
 }
