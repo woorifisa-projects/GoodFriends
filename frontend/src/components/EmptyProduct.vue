3ea89ede708c638e93a06d7dc839e06caa09eb2e
han0224 <dbn0224@gmail.com>
2023-08-31 11:21:36 +0900

    feat: 아무런 상품이 없을 경우 이에 대한 안내 컴포넌트

---
+++ frontend/src/components/EmptyProduct.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="empty">
+    <p>찾으시는 상품이 없어요 ㅠ.ㅠ</p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.empty {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 100px 0;
+}
+.empty > p {
+  font-size: 32px;
+  font-family: 'LINESeedKR-Bd';
+}
+</style>
