a78a5bc7a36c0732ee898d0eed3e754a22515535
devfancy <fancy.junyongmoon@gmail.com>
2023-08-31 12:24:39 +0900

    feat: 프로필 정보를 수정하는 api 기능 구현
    - 프로필 이미지를 수정하는 api
    - 닉네임, 핸드폰, 주소를 수정하는 api
    
    Co-Authored-By: yybeen <59864345+yybeen@users.noreply.github.com>

--- backend/src/main/java/woorifisa/goodfriends/backend/user/application/UserService.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/user/application/UserService.java
@@ -2,22 +2,45 @@ package woorifisa.goodfriends.backend.user.application;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import woorifisa.goodfriends.backend.global.application.S3Service;
+import woorifisa.goodfriends.backend.global.config.utils.FileUtils;
 import woorifisa.goodfriends.backend.user.domain.User;
 import woorifisa.goodfriends.backend.user.domain.UserRepository;
 import woorifisa.goodfriends.backend.user.dto.response.UserResponse;
 import woorifisa.goodfriends.backend.user.exception.NoSuchUserException;
+
+import java.io.IOException;
 
 @Transactional
 @Service
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private final S3Service s3Service;
+
+    public UserService(final UserRepository userRepository, final S3Service s3Service) {
         this.userRepository = userRepository;
+        this.s3Service = s3Service;
     }
+
     public UserResponse findById(final Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(NoSuchUserException::new);
         return new UserResponse(user);
+    }
+
+    public void saveProfileImage(final Long userId, final MultipartFile multipartFile) throws IOException {
+        User foundUser = userRepository.getById(userId);
+        String saveImageUrl =  saveProfileImage(multipartFile);
+
+        foundUser.updateProfileImageUrl(saveImageUrl);
+        userRepository.save(foundUser);
+    }
+    private String saveProfileImage(MultipartFile image) throws IOException {
+        String uniqueFileName = FileUtils.generateUniqueFileName(image.getOriginalFilename());
+        String savedImageUrl = s3Service.saveFile(image, uniqueFileName);
+
+        return savedImageUrl;
     }
 }
