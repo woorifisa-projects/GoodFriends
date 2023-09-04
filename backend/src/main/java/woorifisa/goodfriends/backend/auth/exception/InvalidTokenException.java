a991699106020cce512d0339d716be80a2ddded5
devfancy <fancy.junyongmoon@gmail.com>
2023-09-04 14:54:43 +0900

    refactor: ControllerAdvice 클래스로 예외 처리를 관리했기 때문에 필요없는 어노테이션 제거

--- backend/src/main/java/woorifisa/goodfriends/backend/auth/exception/InvalidTokenException.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/auth/exception/InvalidTokenException.java
@@ -1,9 +1,5 @@
 package woorifisa.goodfriends.backend.auth.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class InvalidTokenException extends RuntimeException {
 
     public InvalidTokenException(final String message) {
