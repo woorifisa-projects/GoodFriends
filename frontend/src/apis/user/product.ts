2b9ee72930e46d62bc3988da4ab8653b3b5d0c65
han0224 <dbn0224@gmail.com>
2023-09-04 11:52:22 +0900

    feat: 상품 수정 및 수정하기 위한 데이터 가져오는 api 연결

--- frontend/src/apis/user/product.ts
+++ frontend/src/apis/user/product.ts
@@ -1,7 +1,7 @@
 import { apiInstance, headers } from '..';
 import { ApiType } from '@/constants/apiType';
 import type { INoContent, IResultType } from '@/types/api';
-import type { IDetailProduct, IProduct } from '@/types/product';
+import type { IDetailProduct, IEditProduct, IProduct } from '@/types/product';
 import { AxiosError, type AxiosResponse } from 'axios';
 
 const api = apiInstance();
@@ -10,7 +10,9 @@ const productAPI = {
   endPoint: {
     getAll: `api/products/view`,
     postProduct: `api/products`,
-    getProduct: `api/products/view/`
+    getProduct: `api/products/view/`,
+    getEditProduct: `api/products/edit/`,
+    editProduct: `api/products/edit/`
   },
   headers: {},
   getAll: (): Promise<IResultType<Array<IProduct>>> => {
@@ -48,6 +50,40 @@ const productAPI = {
       })
       .catch((error: AxiosError) => {
         return { isSuccess: false, message: error.message, type: ApiType.PRODUCT };
+      });
+  },
+  getEditProduct: (token: string, productId: string): Promise<IResultType<IEditProduct>> => {
+    return api
+      .get(productAPI.endPoint.getEditProduct + productId, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then((res: AxiosResponse) => {
+        console.log(res);
+        return { isSuccess: true, data: res.data, type: ApiType.PRODUCT };
+      })
+      .catch((error: AxiosError) => {
+        console.log(error);
+        return { isSuccess: false, message: error.message, type: ApiType.PRODUCT };
+      });
+  },
+  editProduct: (token: string, productId: string, formData: FormData): Promise<INoContent> => {
+    return api
+      .put(productAPI.endPoint.editProduct + productId, formData, {
+        headers: {
+          ...headers,
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then((res: AxiosResponse) => {
+        console.log(res);
+        return { isSuccess: true, message: '' };
+      })
+      .catch((error: AxiosError) => {
+        console.log(error);
+        return { isSuccess: false, message: error.message };
       });
   }
 };
