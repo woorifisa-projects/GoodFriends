87c05f0dcda5caae0b7e0f322aea9beae6845bef
Hmini0101 <hmp9511@naver.com>
2023-09-05 10:16:01 +0900

    refactor: 변수명 변경, 인풋 데이터 타입 수정, 테이블 컴포넌트 전달값 확인

--- frontend/src/views/admin/ManageLogView.vue
+++ frontend/src/views/admin/ManageLogView.vue
@@ -2,7 +2,7 @@
   <div id="total">
     <div class="table">
       <div class="table-in">
-        <Table :data="testData" :header-text="tableHeader" :dataKey="tableDataKey" />
+        <Table :data="data" :header-text="tableHeader" :dataKey="tableDataKey" />
       </div>
     </div>
   </div>
@@ -11,49 +11,36 @@
 <script setup lang="ts">
 import Table from '@/components/CommonTable.vue';
 import manageLogAPI from '@/apis/admin/log';
+import type { IDataType } from '@/types/table';
 
-const test = await manageLogAPI.selectLog();
-// console.log(test);
-// const tableDataKey = [];
-const testData: any[] = [];
-if (test.isSuccess === true && test.data) {
-  // console.log('test: ', test.data);
-  const data = test.data;
+const logApi = await manageLogAPI.selectLog();
 
-  for (let i = 0; i < data.length; i++) {
-    const item = data[i]; // data 배열의 각 항목을 가져옵니다.
+const data: Array<IDataType> = [];
+
+if (logApi.isSuccess === true && logApi.data) {
+  const temp = logApi.data;
+  for (let i = 0; i < temp.length; i++) {
+    const item = temp[i];
 
     let dataObject = {
-      id: (i + 1).toString(), // 1부터 시작하도록 수정합니다.
+      id: i + 1,
       email: item.email,
       nickname: item.nickname,
       ban: item.banCount.toString(),
       last_modified_at: item.lastModifiedAt
     };
-    testData.push(dataObject);
-    // console.log(testData);
+    data.push(dataObject);
   }
 }
-console.log(testData);
-//TODO: api 로그데이터 가져오기
-const tableDataKey = ['id', 'email', 'nickname', 'ban', 'last_modified_at'];
+//데이터 확인 콘솔 console.log(data);
+
+const tableDataKey = ['email', 'nickname', 'ban', 'last_modified_at'];
 const tableHeader = {
-  id: 'zz',
   email: '계정',
   nickname: '닉네임',
   ban: '신고당한 횟수',
   last_modified_at: '마지막 접속날짜'
 };
-// testData = [
-//   {
-//     id: 1,
-//     user_id: 'phm9511',
-//     nickname: 'ddddd',
-//     banCount: '4',
-//     authCheck: '인증완료',
-//     date: '2023-02-02'
-//   }
-// ];
 </script>
 
 <style scoped>
