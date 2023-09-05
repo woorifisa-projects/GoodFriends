656b89d42be1c76e1087d6bb4232d6547a92916f
han0224 <dbn0224@gmail.com>
2023-09-05 12:15:24 +0900

    refactor: 불필요한 주석 삭제

--- frontend/src/apis/admin/log.ts
+++ frontend/src/apis/admin/log.ts
@@ -1,28 +1,26 @@
 import axios, { AxiosError, type AxiosResponse } from 'axios';
-// import type { IResultType, IAdminLoginResponse, IAdminLoginRequest, IGetUserLog } from '@/types/api';
-import type { IResultType, IAdminLoginResponse, IAdminLoginRequest, IGetUserLog } from '@/types/api';
+import type { IResultType, IGetUserLog } from '@/types/api';
 import { ApiType } from '@/constants/apiType';
 import { apiInstance } from '..';
 
 const api = apiInstance();
 
 const manageLogAPI = {
-endPoint: {
+  endPoint: {
     userlog: 'api/admin/user-log/record'
-},
-headers:{},
-selectLog:():Promise<IResultType<Array<IGetUserLog>>> => {
-        return api.get(manageLogAPI.endPoint.userlog)
-        .then((res:AxiosResponse)=>{
+  },
+  headers: {},
+  selectLog: (): Promise<IResultType<Array<IGetUserLog>>> => {
+    return api
+      .get(manageLogAPI.endPoint.userlog)
+      .then((res: AxiosResponse) => {
         const data = res.data;
-        return {isSuccess: true, data:data.userLogRecord, type: ApiType.ADMIN_LOG}
-    
-    }).catch((error: AxiosError) =>{
-        return{isSuccess:false, message: error.message, type: ApiType.ADMIN_LOG}
-    })
-
+        return { isSuccess: true, data: data.userLogRecord, type: ApiType.ADMIN_LOG };
+      })
+      .catch((error: AxiosError) => {
+        return { isSuccess: false, message: error.message, type: ApiType.ADMIN_LOG };
+      });
   }
 };
 
-  
 export default manageLogAPI;
