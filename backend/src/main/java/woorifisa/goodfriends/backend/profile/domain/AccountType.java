c04a751150393ce0ed26962b36a1198047a3ffe5
devfancy <fancy.junyongmoon@gmail.com>
2023-09-07 16:12:10 +0900

    feat: 계좌종류에 대한 `AccountType` enum 클래스 생성

---
+++ backend/src/main/java/woorifisa/goodfriends/backend/profile/domain/AccountType.java
@@ -0,0 +1,12 @@
+package woorifisa.goodfriends.backend.profile.domain;
+
+public enum AccountType {
+    // 국민, 신한, 하나, 우리, 농협, 카카오, 토스
+    KOOKMIN,
+    SHINHAN,
+    HANA,
+    WOORI,
+    NH_NONGHYUP,
+    KAKAO,
+    TOSS
+}
