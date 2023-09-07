b8700bbfa6426a2b634aa8286e941e79a9b20b08
devfancy <fancy.junyongmoon@gmail.com>
2023-09-07 19:08:31 +0900

    feat: 부정행위자 엔티티 생성

---
+++ backend/src/main/java/woorifisa/goodfriends/backend/offender/domain/Offender.java
@@ -0,0 +1,47 @@
+package woorifisa.goodfriends.backend.offender.domain;
+
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import woorifisa.goodfriends.backend.global.common.BaseCreateTimeEntity;
+import woorifisa.goodfriends.backend.user.domain.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@Table(name = "offenders")
+@Entity
+public class Offender extends BaseCreateTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id") // 외래 키로 사용할 컬럼 지정
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    private LocalDateTime limitedDate;
+
+    protected Offender() {
+    }
+
+    public Offender(User user, LocalDateTime limitedDate) {
+        this.user = user;
+        this.limitedDate = limitedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public LocalDateTime getLimitedDate() {
+        return limitedDate;
+    }
+}
