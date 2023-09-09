8ab03c7db01cfa1fc366d3d099ad3a0b659afac4
devfancy <fancy.junyongmoon@gmail.com>
2023-09-09 14:50:57 +0900

    feat: 신고에 대한 값을 매핑하기 위한 stirngs 폴더안에 report.ts 추가

---
+++ frontend/src/constants/strings/report.ts
@@ -0,0 +1,16 @@
+export const REPORT = {
+  TITLE_PRODUCT: '상품 신고',
+  CIRCLE_ONE: '1',
+  CIRCLE_TWO: '2',
+  SEMI_TITLE_ONE: '아래에 신고 사유를 선택해 주세요.',
+  SEMI_TITLE_TWO: '해당 게시글을 신고하는 이유를 적어주세요.',
+  CATEGORY_CONTENT_ONE: '판매 금지 물품이에요.',
+  CATEGORY_CONTENT_TWO: '중고거래 게시글이 아니에요.',
+  CATEGORY_CONTENT_THREE: '전문 판매업자 같아요.',
+  CATEGORY_CONTENT_FOUR: '사기 글이에요.',
+  CATEGORY_CONTENT_FIVE: '거래 / 환불 분쟁',
+  CATEGORY_CONTENT_ETC: '기타 사유',
+  REPORT_CONTENT: '신고 내용',
+  REPORT_DETAIL: '신고 내용을 적어주세요',
+  BUTTON: '다음'
+};
