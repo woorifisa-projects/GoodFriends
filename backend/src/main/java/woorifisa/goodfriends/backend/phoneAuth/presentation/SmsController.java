2dc3a1185b84a21bf9036f34ca18d947112d6d37
Hmini0101 <hmp9511@naver.com>
2023-09-10 13:35:55 +0900

    feat: 인증번호 검증 api 추가

--- backend/src/main/java/woorifisa/goodfriends/backend/phoneAuth/presentation/SmsController.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/phoneAuth/presentation/SmsController.java
@@ -3,6 +3,7 @@ package woorifisa.goodfriends.backend.phoneAuth.presentation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import woorifisa.goodfriends.backend.phoneAuth.application.CreatePhoneAuthNum;
 import woorifisa.goodfriends.backend.phoneAuth.application.SmsService;
 import woorifisa.goodfriends.backend.phoneAuth.dto.request.Request;
 import woorifisa.goodfriends.backend.phoneAuth.dto.response.SmsResponse;
@@ -14,17 +15,25 @@ import java.security.NoSuchAlgorithmException;
 @RequestMapping("/api/sms")
 public class SmsController {
 
-    private SmsService smsService;
+    private final SmsService smsService;
     private Request request;
-
+    private CreatePhoneAuthNum createPhoneAuthNum;
     public SmsController(SmsService smsService) {
         this.smsService = smsService;
     }
 
-    @PostMapping("/user/{phoneNumber}")
-    public ResponseEntity<SmsResponse> test(@PathVariable String phoneNumber) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+    @PostMapping("/user")
+    public void phoneNumberAuth(@RequestBody Request request) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException, JsonProcessingException {
+        this.createPhoneAuthNum = new CreatePhoneAuthNum();
+        SmsResponse data = smsService.sendSms(request.getRecipientPhoneNumber(),createPhoneAuthNum.getAuthNumber());
+//        return ResponseEntity.ok().body(data);
 
-        SmsResponse data = smsService.sendSms(phoneNumber);
-        return ResponseEntity.ok().body(data);
+    }
+
+    @GetMapping("/user/check/{inputAuthNum}")
+    public String receiveAuthNumber (@PathVariable int inputAuthNum){
+        String result = smsService.checkPhoneAuthNum(inputAuthNum,createPhoneAuthNum.getAuthNumber());
+
+        return result;
     }
 }
