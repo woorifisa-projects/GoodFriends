f2606c459d047718f510983103f339e9c078f301
han0224 <dbn0224@gmail.com>
2023-09-13 18:58:15 +0900

    style: 배너 변경

--- frontend/src/utils/image.ts
+++ frontend/src/utils/image.ts
@@ -1,11 +1,9 @@
-import banner1 from '@/assets/tmp/images/banner1.jpeg';
-import banner2 from '@/assets/tmp/images/banner2.jpeg';
-import banner3 from '@/assets/tmp/images/banner3.jpeg';
+import banner1 from '@/assets/images/banner1.png';
+import banner2 from '@/assets/images/banner2.png';
 import image from '@/assets/tmp/images/image.png';
-import banner4 from '@/assets/tmp/images/banner4.png';
 
 export const getBannerList = () => {
-  return [banner1, banner2, banner3, banner4];
+  return [banner1, banner2];
 };
 
 export const tmpImage = image;
