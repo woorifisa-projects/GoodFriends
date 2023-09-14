46703c3113a06f4c6d3740091df7d4e9f679a74f
devfancy <fancy.junyongmoon@gmail.com>
2023-09-14 11:30:04 +0900

    refactor: sameSite 설정을 위해 ResponseCookie
    - Chome에 Cookies에 refresh token 값을 가져오기 위해 sameSite을 None으로 설정
    - 프론트와 백엔드 도메인 주소가 다를 경우 sameSite를 None으로 설정해야함

--- backend/src/main/java/woorifisa/goodfriends/backend/auth/application/AuthTokenResponseHandler.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/auth/application/AuthTokenResponseHandler.java
@@ -2,9 +2,9 @@ package woorifisa.goodfriends.backend.auth.application;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
@@ -18,24 +18,26 @@ public class AuthTokenResponseHandler {
     }
 
     public void setRefreshTokenCookie(String refreshToken) {
-        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
-        // HttpOnly 설정시 클라이언트에서 쿠키에 직접 js 코드로 접근이 불가능하고
-        // 이는 refreshToken 유효성 검사시 서버의 네트워크 통신을 한번 더 요구하고 추가 API 작성 및 클라이언트 OAuth 로직에서 복잡성을 유발하여
-        // 현 기준 HttpOnly 옵션은 일단 사용을 안하는 것으로 결정
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(true); // https 옵션 설정
-        refreshTokenCookie.setMaxAge(14 * 24 * 60 * 60); // 리프레시 토큰 유효 기간 설정 (14일)
-        refreshTokenCookie.setPath("/"); // 쿠키의 유효 경로 설정 (애플리케이션 전체)
-        httpServletResponse.addCookie(refreshTokenCookie);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None") // SameSite 설정을 None으로 추가
+                .maxAge(14 * 24 * 60 * 60) // 리프레시 토큰 유효 기간 설정 (14일)
+                .path("/") // 쿠키의 유효 경로 설정 (애플리케이션 전체)
+                .build();
+
+        httpServletResponse.addHeader("Set-Cookie", refreshTokenCookie.toString());
     }
 
     public void setRefreshTokenDeleteCookie(String refreshToken){
-        // 브라우저에 저장되어 있던 refreshToken의 만료시간을 0으로 설정해 바로 refreshToken이 만료되도록
-        Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
-        refreshTokenCookie.setHttpOnly(true);
-        refreshTokenCookie.setSecure(true);
-        refreshTokenCookie.setMaxAge(0);
-        refreshTokenCookie.setPath("/");
-        httpServletResponse.addCookie(refreshTokenCookie);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None") // SameSite 설정을 None으로 추가
+                .maxAge(0)
+                .path("/")
+                .build();
+
+        httpServletResponse.addHeader("Set-Cookie", refreshTokenCookie.toString());
     }
 }
