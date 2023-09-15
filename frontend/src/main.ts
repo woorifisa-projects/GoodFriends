09af32a3096b7f4d19f4e8dfd4ef7e214cc31fd2
han0224 <dbn0224@gmail.com>
2023-09-15 18:04:10 +0900

    feat: aos 라이브러리 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,14 +2,17 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import 'aos/dist/aos.css';
 
 import App from './App.vue';
 import router from './router';
-
 import 'material-icons/iconfont/material-icons.css';
+
+import AOS from 'aos';
 
 const app = createApp(App);
 
+AOS.init();
 app.use(createPinia());
 app.use(router);
 
