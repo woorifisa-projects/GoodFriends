e5e3bbce2a7c178b65b26a6352be0f01078d5734
devfancy <fancy.junyongmoon@gmail.com>
2023-12-19 16:50:34 +0900

    refactor: String 값으로 변환하도록 수정

--- backend/src/main/java/woorifisa/goodfriends/backend/report/domain/ReportCategory.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/report/domain/ReportCategory.java
@@ -1,10 +1,17 @@
 package woorifisa.goodfriends.backend.report.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ReportCategory {
-    PROHIBITED_GOODS, // 판매금지 물품이에요
-    USED_TRANSACTION_POST, // 중고 거래 게시글이 아니에요
-    PROFESSIONAL_VENDOR, // 전문 판매업자 같아요
-    FRAUDULENT_WRITING, // 사기 글이에요
-    TRANSACTION_AND_REFUND_DISPUTE, // 거래/환불 분쟁
-    OTHER_REASONS, // 기타 사유 선택
+    PROHIBITED_GOODS("판매금지 물품이에요"),
+    USED_TRANSACTION_POST("중고 거래 게시글이 아니에요"),
+    PROFESSIONAL_VENDOR("전문 판매업자 같아요"),
+    FRAUDULENT_WRITING("사기 글이에요"),
+    TRANSACTION_AND_REFUND_DISPUTE("거래/환불 분쟁"),
+    OTHER_REASONS("기타 사유 선택");
+
+    private final String text;
 }
--- backend/src/main/java/woorifisa/goodfriends/backend/report/domain/ReportStatus.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/report/domain/ReportStatus.java
@@ -1,6 +1,12 @@
 package woorifisa.goodfriends.backend.report.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ReportStatus {
-    PROCESSING, // 처리중
-    COMPLETED, // 처리 완료
+    PROCESSING("처리중"),
+    COMPLETED("처리 완료");
+    private final String text;
 }
