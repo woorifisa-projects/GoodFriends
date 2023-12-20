b70ac3e40e50b780e7f74217f757a5ceb4c769bd
devfancy <fancy.junyongmoon@gmail.com>
2023-12-20 15:45:49 +0900

    refactor: ReportService에서 트랜잭션 처리 리팩터링
    - 여러 개의 신고로 인해 부정행위자를 생성하는 로직을 'createOffenderForViolation'이라는 별도의 메서드로 추출.
    - 'createOffenderForViolation' 메서드를 'saveReport'에서 제공하는 기존 트랜잭션 컨텍스트 내에서 동작하도록 조정.
    - 다른 메서드에서는 이미 'saveReport' 트랜잭션의 일부이므로 불필요한 @Transactional 어노테이션을 제거.
    - 명료성을 위해 메서드 이름과 주석을 업데이트.
    - 불필요한 행 제거

--- backend/src/main/java/woorifisa/goodfriends/backend/report/application/ReportService.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/report/application/ReportService.java
@@ -26,9 +26,7 @@ public class ReportService {
     private final UserRepository userRepository;
     private final ProductRepository productRepository;
     private final ReportRepository reportRepository;
-
     private final OffenderRepository offenderRepository;
-
     private final ProfileRepository profileRepository;
 
     public ReportService(final UserRepository userRepository,
@@ -45,42 +43,17 @@ public class ReportService {
 
     @Transactional
     public Long saveReport(final LoginUser loginUser, final Long productId, final BoardReportCreateServiceRequest request) {
-
-        // 프로필 미등록자 신고 불가
-        if(!existProfile(loginUser.getId())) {
+        if(!existProfile(loginUser.getId()))
             throw new NotFoundProfile();
-        }
-
-        // 중복 신고불가
-        if(duplicateReport(productId, loginUser.getId())) {
+        if(duplicateReport(productId, loginUser.getId()))
             throw new AlreadyReportedException();
-        }
 
-        // 신고 등록하면 신고 테이블 (신고한 유저, 신고 카테고리, 신고 내용) 생성
         User foundUser = userRepository.getById(loginUser.getId());
-        Product foundProduct = productRepository.getByProductIdAndUserId(productId);
-
-        Report newReport = createReport(foundUser, foundProduct, request);
+        Product productCreator = productRepository.getByProductIdAndUserId(productId);
+        Report newReport = createReport(foundUser, productCreator, request);
         reportRepository.save(newReport);
 
-        // 신고 당한 유저는 횟수 + 1 증가
-        int updatedBan = foundProduct.getUser().getBan() + REPORT_INCREMENT_COUNT;
-        foundProduct.getUser().updateBan(updatedBan);
-
-
-        // 신고 당한 유저의 ban 횟수가 3번 이상이면 비활성화 상태로 변경
-        if(updatedBan >= MAX_REPORT_BAN_COUNT) {
-            boolean notActivated = false;
-            foundProduct.getUser().updateActivated(notActivated);
-
-            Offender offender = createOffender(foundProduct.getUser());
-
-            // 신고 당한 횟수가 3번 이상일 때 부정행위자 테이블에 추가
-            offenderRepository.save(offender);
-        }
-
-        // 신고당한 유저의 신고 당한 횟수 업데이트
-        userRepository.save(foundProduct.getUser());
+        createOffenderForViolation(productCreator);
         return newReport.getId();
     }
 
@@ -93,16 +66,26 @@ public class ReportService {
         Report report = reportRepository.findByProductIdAndUserId(productId, userId);
         return report != null;
     }
-    @Transactional
     public Report createReport(final User user, final Product product , final BoardReportCreateServiceRequest request) {
         Report newReport = request.toEntity(user, product);
         return newReport;
     }
 
-    @Transactional
+    public void createOffenderForViolation(final Product productCreator) {
+        int incrementedReportCount = productCreator.getUser().getBan() + REPORT_INCREMENT_COUNT;
+        productCreator.getUser().updateBan(incrementedReportCount);
+
+        if(incrementedReportCount >= MAX_REPORT_BAN_COUNT) {
+            boolean notActivated = false;
+            productCreator.getUser().updateActivated(notActivated);
+            Offender offenderUser = createOffender(productCreator.getUser());
+            offenderRepository.save(offenderUser);
+        }
+        userRepository.save(productCreator.getUser());
+    }
+
     public Offender createOffender(final User offenderUser) {
         Offender existingOffender = findOffender(offenderUser);
-
         if(existingOffender != null) {
             return existingOffender;
         }
