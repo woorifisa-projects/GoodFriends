ea247364b0310aec86135834e132b002d3760299
devfancy <fancy.junyongmoon@gmail.com>
2023-12-22 23:59:46 +0900

    refactor: EnableJpaAuditing 어노테이션을 적용하는 클래스를 config 패키지에 생성

---
+++ backend/src/main/java/woorifisa/goodfriends/backend/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package woorifisa.goodfriends.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing // 2
+@Configuration
+public class JpaAuditingConfig {
+}
