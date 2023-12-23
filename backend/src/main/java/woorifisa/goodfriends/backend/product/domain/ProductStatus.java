a805e323054345a7ded04846e671907aa3dcb199
devfancy <fancy.junyongmoon@gmail.com>
2023-12-23 16:16:51 +0900

    refactor: ProductStatus 클래스 가독성있게 수정
    - 값이 있는 생성자는 RequiredArgsConstructor 어노테이션을 사용하여 생성

--- backend/src/main/java/woorifisa/goodfriends/backend/product/domain/ProductStatus.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/product/domain/ProductStatus.java
@@ -1,10 +1,15 @@
 package woorifisa.goodfriends.backend.product.domain;
 
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ProductStatus {
-    // 판매중(기본값)
-    SELL,
-    // 예약중
-    RESERVATION,
-    // 거래완료
-    COMPLETED
+    SELL("판매중"), // 기본값
+    RESERVATION("예약중"),
+    COMPLETED("거래완료");
+
+    private final String text;
 }
