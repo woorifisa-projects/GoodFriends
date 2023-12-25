deacafce3e6c614114e2c255b2c30c2ebf893f8a
devfancy <fancy.junyongmoon@gmail.com>
2023-12-25 20:12:52 +0900

    chore: 네이티브 SQL 쿼리와 Hibernate 쿼리에 대해 읽기 쉽게 코드 정렬

--- backend/src/main/java/woorifisa/goodfriends/backend/product/domain/ProductImageRepository.java
+++ backend/src/main/java/woorifisa/goodfriends/backend/product/domain/ProductImageRepository.java
@@ -9,16 +9,24 @@ import java.util.List;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
 
-    @Query(value = "select pi.image_url from product_images pi where pi.product_id = :productId limit 1", nativeQuery = true)
+    @Query(value = "" +
+            "select pi.image_url " +
+            "from product_images pi " +
+            "where pi.product_id = :productId limit 1", nativeQuery = true)
     String findOneImageUrlByProductId(@Param("productId") final Long productId);
 
-    @Query("select pi.imageUrl from ProductImage pi where pi.product.id = :productId")
+    @Query("" +
+            "select pi.imageUrl " +
+            "from ProductImage pi " +
+            "where pi.product.id = :productId")
     List<String> findAllImageUrlByProductId(@Param("productId") final Long productId);
 
     List<ProductImage> findByProductId(final Long productId);
 
     @Modifying(clearAutomatically = true)
-    @Query(value = "DELETE FROM product_images WHERE product_id = :productId", nativeQuery = true)
-    void deleteByProductId(final @Param("productId") Long productId);
+    @Query(value = "" +
+            "DELETE FROM product_images " +
+            "WHERE product_id = :productId", nativeQuery = true)
+    void deleteByProductId(@Param("productId") final Long productId);
 
 }
